e5ffda23123ca5e4e62fe2d52aa2262b042afb1a
Arnav G <soccercream20@gmail.com>
2020-12-27 20:58:26 -0800

    added rgb

--- detect.py
+++ detect.py
@@ -4,6 +4,7 @@ from tensorflow import keras
 import numpy
 import os
 from tensorflow.python.keras import models
+os.chdir("facialrecognition")
 model = models.load_model('model.h5')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
@@ -16,18 +17,17 @@ while True:
         cord1 = (x,y)
         cord2 = (x+w,y+h)
 
-    cv2.rectangle(imgs, cord1, cord2, (255, 255, 255), 2)
+        cv2.rectangle(imgs, cord1, cord2, (255, 255, 255), 2)
 
-    img = imgs[cord1[1]:cord2[1], cord1[0]:cord2[0]]
-    unmodded = img
-    img = cv2.resize(img,(256,256))
-    img = tf.image.rgb_to_grayscale(img)
-    pl = []
-    pl.append(img)
-    pl = numpy.asarray(pl)
-    predictions = model.predict(pl)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    strg = categories[int(numpy.argmax(predictions[0]))]
-    cv2.putText(imgs,strg,cord1, font, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+        img = imgs[cord1[1]:cord2[1], cord1[0]:cord2[0]]
+        unmodded = img
+        img = cv2.resize(img,(256,256))
+        pl = []
+        pl.append(img)
+        pl = numpy.asarray(pl)
+        predictions = model.predict(pl)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strg = categories[int(numpy.argmax(predictions[0]))]
+        cv2.putText(imgs,strg,cord1, font, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.imshow("Collecting images", imgs)
     cv2.waitKey(1)
---
+++ face.py
@@ -0,0 +1,18 @@
+
+import os
+import cv2
+os.chdir("facialrecognition")
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+kjl = 500
+for img in os.listdir("main"):
+    imgl = cv2.imread("main/"+img)
+    gray = cv2.cvtColor(imgl, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cord1 = (x,y)
+        cord2 = (x+w,y+h)
+        cv2.rectangle(imgl, cord1, cord2, (255, 255, 255), 2)
+        img = imgl[cord1[1]:cord2[1], cord1[0]:cord2[0]]
+        cv2.imwrite("face/"+str(kjl)+".jpg",img)
+        kjl+=1
+print("DONE")
